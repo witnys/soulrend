de4204e314df33f1b2ef4a7a88289c739de0a2d4
witnys <104702118+witnys@users.noreply.github.com>
2022-04-30 19:45:42 -0500

    Initial commit

---
+++ ValheimModTemplate/ValheimModTemplate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace $safeprojectname$
+{
+    [BepInPlugin(ID, title, version)]
+    public class ValheimModTemplate : BaseUnityPlugin
+    {
+        public const string ID = "";
+        public const string version = "0.0.0.0";
+        public const string title = "Valheim Mod Template";
+
+        public Harmony harmony;
+        
+        public static BepInEx.Logging.ManualLogSource harmonyLog;
+
+        public void Awake()
+        {
+            harmony = new Harmony(ID);
+            harmony.PatchAll();
+            harmonyLog = Logger;
+
+            harmonyLog.LogDebug("$safeprojectname$ loaded.");
+        }
+    }
+
+    #region Utils
+
+    #endregion
+
+    #region Transpilers
+
+    #endregion
+
+    #region Patches 
+
+    #endregion
+}
